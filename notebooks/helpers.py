aa93de1ea573a3ebfd057df6e68a6b6105d743fc
Pilipolio <guillaume.allain@gmail.com>
2013-09-28 11:17:32 +0100

    Moved HtmlScripts to notebooks/helpers.

---
+++ notebooks/helpers.py
@@ -0,0 +1,54 @@
+from IPython.display import display, HTML, Javascript
+
+class HtmlScript(object):
+    def __init__(self, div_id, js_lib=""):
+        self.div_id = div_id
+        self.js_lib = js_lib
+        self.css_list = []
+        self.vars = {}
+        self.js = ['']
+        self.html = ''
+
+    def update_vars(self, **kw):
+        # could do some (recursive) checking for keys and values here (as used to do convertVar)
+        self.vars.update(**kw)
+
+    def convert_value(self, value):
+        value_type = type(value)
+        if value_type == str:
+            return '"%s"' % value
+        elif value_type in (list, dict, int, float) or value == None:
+            return str(value)
+        else:
+            print typeOut
+            raise TypeError
+    
+    def get_js_vars_lines(self):
+        return ["var {0}={1};".format(k,self.convert_value(v)).replace("u'", "'") 
+                for k,v in self.vars.iteritems()]
+
+    def add_js(self, jsStr):
+        if type(jsStr) != str:
+           raise TypeError
+        self.js.append(jsStr)
+
+    def add_css(self, css):
+        if type(css) != str:
+           raise TypeError
+        else:
+            self.css_list.append(css)
+       
+    def get_html_only_lines(self):
+        return ['<style>'] + self.css_list + ['</style>', '<div id="{div_id}">'.format(div_id=self.div_id), self.html, '</div>']
+
+    def get_html_only(self):
+        return '\n'.join(self.get_html_only_lines())
+
+    def get_js(self):
+        return '\n'.join(self.get_js_vars_lines() + self.js)
+    
+    def to_string(self):
+        return '<script>\n' + self.get_js() + '</script>'
+    
+    def display(self):
+        display(HTML(self.get_html_only()), Javascript(self.get_js(), lib=self.js_lib))
